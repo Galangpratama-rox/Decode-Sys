413972ee1a722479dd3e7375fcd36c7f71721640
Muhammad Galang <galangm504@gmail.com>
2026-09-15 22:36:13 +0700

    add syshub v2 loader with script_key bypass

---
+++ lua/syshub_v2_loader.lua
@@ -0,0 +1,7 @@
+-- SysHub V2 Loader
+-- Set _G.script_key lalu loadstring loader langsung dari syshub.site
+-- Tidak perlu intercept, tidak perlu decode manual
+
+_G.script_key = "FREE-SYS-TCFA-OWWQ-OBKY"
+
+loadstring(game:HttpGet("https://syshub.site/loader", true))()
